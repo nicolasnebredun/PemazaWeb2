0ff0fef14cb17b050ac1e9b9afdc66f046cb67f4
NicolasNeto <nicolasd1545@gmail.com>
2024-07-18 20:36:57 -0400

    search on lojas

---
+++ search/search.js
@@ -0,0 +1,34 @@
+function searchLoja() {
+  const debounce = (func, delay) => {
+    let timeoutId;
+    return function (...args) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => func.apply(this, args), delay);
+    };
+  };
+
+  const input = document.getElementById("filter").value.toLowerCase();
+  const cardContainer = document.getElementById("cardLojas");
+  const cards = Array.from(cardContainer.getElementsByClassName("card"));
+
+  const needle = input.slice(0, 5).toLowerCase();
+  let found = false;
+  for (const card of cards) {
+    const title = card.querySelector(".text-title").innerText.toLowerCase();
+    card.style.transition = "opacity 0.1s ease-in-out";
+    if (title.includes(needle)) {
+      found = true;
+      card.style.opacity = "1";
+      card.style.display = "";
+    } else {
+      card.style.opacity = "0";
+      card.style.display = "none";
+    }
+  }
+
+  if (!found) {
+    alert("Cidade não Encontrada, Tente Novamente!");
+  }
+
+  searchLoja = debounce(searchLoja, 300);
+}
